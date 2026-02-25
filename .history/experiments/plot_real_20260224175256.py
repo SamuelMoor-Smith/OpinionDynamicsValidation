762dbe10ac49657edbbd8c92956c9b9c72f11eda
Samuel Moor-Smith <samuelmoorsmith@gmail.com>
2026-02-24 21:24:27 -0800

    before readme

--- .history/experiments/plot_real_20260224175256.py
+++
@@ -1,4 +0,0 @@
-from utils.plotting.plotting import produce_stripplot, produce_figure
-
-# produce_stripplot()
-produce_figure("deffuant_with_repulsion", "deffuant_with_repulsion", "./results/optimized/deffuant_with_repulsion/deffuant_with_repulsion_20250630_123836.jsonl", "optimized")
